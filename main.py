59a54f384059b2db7e53116b7036af19dad36bfa
a-powers <awpowers@gmail.com>
2021-02-26 14:21:10 +0000

    update

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ student_scores = {
   "Neville": 62,
 }
 
+
+all_grades = {}
+
+for i in student_scores:
+    grades = student_scores[i]
+    if grades > 90:
+        all_grades[i] = "Outstanding"
+    elif grades > 80:
+        all_grades[i] = "Exceeds Expectations"
+    elif grades > 70:
+        all_grades[i] = "Average"
+    elif grades <= 70:
+        all_grades[i] = "Not Passing"
+print(all_grades)
