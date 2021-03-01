c3b128400eab5165c7cb199804fa2f3d3735be2d
a-powers <awpowers@gmail.com>
2021-03-01 14:16:00 +0000

    update

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ student_scores = {
   "Neville": 62,
 }
 
+new_grades = {}
+
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        new_grades[student] = "Outstanding"
+    elif score > 80:
+        new_grades[student] = "Exceeds Average"
+    elif score > 70:
+        new_grades[student] = "Averge"
+    elif score <= 70:
+        new_grades[student] = "Below Average"
+
+print(new_grades)
